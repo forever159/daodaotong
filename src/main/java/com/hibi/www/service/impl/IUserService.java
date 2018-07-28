e3a1e0fa27d6f6200668e5d6d3c0efcfb5d2885c
penglei <3252564019@qq.com>
2018-07-28 11:47:25 +0800

    道道通系统 一、道路应急APP   1、软件基本架构持续更新!

--- src/main/java/com/hibi/www/service/impl/IUserService.java
+++ src/main/java/com/hibi/www/service/impl/IUserService.java
@@ -1,13 +1,46 @@
 package com.hibi.www.service.impl;
+import com.hibi.www.dao.mapper.PermissionMapper;
+import com.hibi.www.dao.mapper.UserMapper;
+import com.hibi.www.domain.Permission;
+import com.hibi.www.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
-public class IUserService{
+public class IUserService implements UserDetailsService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    PermissionMapper permissionMapper;
 
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userMapper.findByUserName(s);
+        if (user != null) {
+            List<Permission> permissions = permissionMapper.findByAdminUserId(user.getId());
+            List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+            for (Permission per:permissions) {
+                if (per != null && per.getName() !=null) {
+                    GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(per.getName());
+                    grantedAuthorities.add(grantedAuthority);
+                }
+            }
+            return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword()
+            ,grantedAuthorities);
+        }else{
+            throw new UsernameNotFoundException("admin: " + s + " do not exist!");
+        }
+    }
 }
